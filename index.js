bb0a0ee89a32450374e47dbb5fcad96468004564
HelioPompei <helioberizonzipompei@hotmail.com>
2022-06-18 17:58:15 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const bodyParser = require("body-parser");
+const express = require("express"); 
+const app = express(); 
+const PORT = 8080;
+const connection = require("./database/Database");
+
+connection.authenticate()
+    .then(() => {
+        console.log("connection authenticated");
+    }).catch((error) => {
+        console.log("connection error: " + error.message);
+    });
+
+// listen on port 8080 
+app.listen(PORT, () => {
+    console.log("listening on port " + PORT);  
+});
+
+// view engine configuration
+app.set("view engine", "ejs");
+
+// static
+app.use(express.static("public"));
+
+// body parser configuration
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req,res) => {
+    res.render("home.ejs");
+});
+
